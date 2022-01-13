e470e98b248786282e4b65f7df4739278448a5fe
alysontrizotto <alysontrizotto1@gmail.com>
2022-01-13 05:16:58 -0300

    Portifólio
    Push do portifólio página home

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const botaoMenu=document.querySelector('.menu_pontilhado')
+const menu = document.querySelector('.menu_lateral')
+
+botaoMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu_lateral--ativo')
+})
