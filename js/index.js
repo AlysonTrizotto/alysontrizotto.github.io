bc011ea3f6193b54d22677b8d304023ef1940bad
alysontrizotto <alysontrizotto1@gmail.com>
2022-01-18 21:13:46 -0300

    Modificado para redimensionamento

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const botaoMenu=document.querySelector('.menu_pontilhado')
 const menu = document.querySelector('.menu_lateral')
 
 botaoMenu.addEventListener('click', () => {
-    menu.classList.toggle('menu_lateral--ativo')
+    menu.classList.toggle('menu_lateral--ativo') 
 })
